d06999a3c68aab8664e763f6bcabc01aef616f46
ptriantafyll <ptriantafyll@gmail.com>
2025-03-24 16:53:12 +0200

    Added temporary welcome page

---
+++ lib/screens/welcome.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Not implemented',
+        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color: Theme.of(context).colorScheme.primary,
+            ),
+      ),
+    );
+  }
+}
