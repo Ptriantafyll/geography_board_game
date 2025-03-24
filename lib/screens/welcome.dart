76ae6cf1d192df87eed99b6f17a6110e8faae3e9
ptriantafyll <ptriantafyll@gmail.com>
2025-03-24 18:41:17 +0200

    Added a couple of buttons

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -6,11 +6,27 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        'Not implemented',
-        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-              color: Theme.of(context).colorScheme.primary,
-            ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Not implemented',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            // style: ElevatedButton.styleFrom(
+            //   backgroundColor: Colors.black,
+            // ),
+            child: const Text("Νέο παιχνίδι"),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("Συνέχισε παλιό παιχνίδι"),
+          ),
+        ],
       ),
     );
   }
