830ad8a358571d65ab1e06897cdaf4134118f0a4
ptriantafyll <ptriantafyll@gmail.com>
2025-04-13 16:36:19 +0300

    Added 3 dummy questions

---
+++ frontend/lib/data/questions.dart
@@ -0,0 +1,14 @@
+final dummy_questions = [
+  {
+    "question": "How tall is mount everest? (in meters)",
+    "answer": 8849,
+  },
+  {
+    "question": "How deep is the marianna trench? (in meters)",
+    "answer": 11020,
+  },
+  {
+    "question": "How much is India's population? ",
+    "answer": 1438000000,
+  }
+];
