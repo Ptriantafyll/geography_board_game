93b762a71ecac0b4ef0746ed3895afd60ebf23ed
ptriantafyll <ptriantafyll@gmail.com>
2025-04-13 18:19:18 +0300

    Updated question card to include answer text field

--- frontend/lib/widgets/question_card.dart
+++ frontend/lib/widgets/question_card.dart
@@ -1,24 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:geography_board_game/models/question.dart';
 
 class QuestionCard extends StatelessWidget {
   const QuestionCard({
     super.key,
     // todo: use GameQuestion model
-    required this.questions,
-    required this.answers,
+    required this.gameQuestion,
+    required this.questionController,
   });
 
-  final List<String> questions;
-  final List<String> answers;
+  final GameQuestion gameQuestion;
+  final TextEditingController questionController;
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      // Maybe make this just return 1 question
-      child: ListView.builder(
-        itemCount: 6,
-        itemBuilder: (ctx, index) => ListTile(
-          title: Text(questions[index]),
+      shadowColor: Colors.black,
+      child: Container(
+        height: 250,
+        width: 250,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Text(gameQuestion.questionText),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: TextField(
+                maxLength: 20,
+                controller: questionController,
+                decoration: InputDecoration(
+                  label: Text('Απάντηση'),
+                ),
+              ),
+            ),
+            // todo: add submit button that sends websocket request to answer question
+          ],
         ),
       ),
     );
