0af30b45dcb9c1415a64eea8653dd1133d045bb9
ptriantafyll <ptriantafyll@gmail.com>
2025-04-13 18:19:44 +0300

    Made dummy answers double

--- frontend/lib/data/questions.dart
+++ frontend/lib/data/questions.dart
@@ -1,14 +1,14 @@
-final List<Map<String, Object>> dummy_questions = [
+final List<Map<String, dynamic>> dummy_questions = [
   {
     "question": "How tall is mount everest? (in meters)",
-    "answer": 8849,
+    "answer": 8849.0,
   },
   {
     "question": "How deep is the marianna trench? (in meters)",
-    "answer": 11020,
+    "answer": 11020.0,
   },
   {
     "question": "How much is India's population? ",
-    "answer": 1438000000,
+    "answer": 1438000000.0,
   }
 ];
