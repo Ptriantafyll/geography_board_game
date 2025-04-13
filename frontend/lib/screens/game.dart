7f91a8a62689157a2fd842554857586a10578111
ptriantafyll <ptriantafyll@gmail.com>
2025-04-13 17:27:20 +0300

    Added content based on the game state

--- frontend/lib/screens/game.dart
+++ frontend/lib/screens/game.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geography_board_game/data/questions.dart';
 import 'package:geography_board_game/models/player.dart';
 import 'package:geography_board_game/widgets/player_item.dart';
 
@@ -13,8 +14,65 @@ class GameScreen extends StatefulWidget {
 }
 
 class _GameScreenState extends State<GameScreen> {
+  final questions = dummy_questions;
+  Map<String, Object>? currentQuestion;
+  bool showingScores = true;
+  bool showingQuestion = false;
+  bool showingAnswers = false;
+
+  void showQuestion() {
+    // todo: get a random question from the db
+
+    if (showingScores) {
+      showingScores = false;
+      showingQuestion = true;
+      showingAnswers = false;
+    } else if (showingQuestion) {
+      showingScores = false;
+      showingQuestion = false;
+      showingAnswers = true;
+    } else if (showingAnswers) {
+      showingScores = true;
+      showingQuestion = false;
+      showingAnswers = false;
+    }
+
+    setState(() {
+      currentQuestion = questions[0];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content;
+
+    if (showingScores) {
+      content = Column(
+        children: [
+          Text('Game ID: ${widget.gameId}'),
+          Expanded(
+            child: ListView.builder(
+              itemCount: widget.players.length,
+              itemBuilder: (ctx, index) => PlayerItem(
+                player: widget.players[index],
+                isGame: true,
+              ),
+            ),
+          ),
+        ],
+      );
+    } else if (showingQuestion) {
+      content = Center(
+        child: Text(currentQuestion!['question'].toString()),
+      );
+    } else if (showingAnswers) {
+      content = Center(
+        child: Text('Showing Answers'),
+      );
+    } else {
+      content = Text('No content yet');
+    }
+
     return Scaffold(
       appBar: AppBar(
         // backgroundColor: Theme.of(context).colorScheme.primary,
@@ -26,20 +84,14 @@ class _GameScreenState extends State<GameScreen> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          Text('Game ID: ${widget.gameId}'),
-          Expanded(
-            // todo: add scores
-            child: ListView.builder(
-              itemCount: widget.players.length,
-              itemBuilder: (ctx, index) => PlayerItem(
-                player: widget.players[index],
-                isGame: true,
-              ),
-            ),
-          ),
-        ],
+      body: content,
+      floatingActionButton: ElevatedButton(
+        onPressed: showQuestion,
+        child: showingScores
+            ? const Text('Next Question')
+            : showingQuestion
+                ? const Text('Show Answers')
+                : const Text('Show Scores'),
       ),
     );
   }
