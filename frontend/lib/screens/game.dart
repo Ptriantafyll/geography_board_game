939edc11c7e8e186c7116761ba0a2dba8998b31d
ptriantafyll <ptriantafyll@gmail.com>
2025-04-14 21:26:20 +0300

    Added submitAnswer functionality

--- frontend/lib/screens/game.dart
+++ frontend/lib/screens/game.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:geography_board_game/data/questions.dart';
 import 'package:geography_board_game/functions/alert.dart';
 import 'package:geography_board_game/models/player.dart';
 import 'package:geography_board_game/models/question.dart';
+import 'package:geography_board_game/models/websocket_response.dart';
+import 'package:geography_board_game/providers/websocket_provider.dart';
 import 'package:geography_board_game/widgets/player_item.dart';
 import 'package:geography_board_game/widgets/question_card.dart';
 
-class GameScreen extends StatefulWidget {
+class GameScreen extends ConsumerStatefulWidget {
   const GameScreen({super.key, required this.gameId, required this.players});
 
   final String gameId;
   final List<Player> players;
 
   @override
-  State<StatefulWidget> createState() => _GameScreenState();
+  ConsumerState<ConsumerStatefulWidget> createState() => _GameScreenState();
 }
 
-class _GameScreenState extends State<GameScreen> {
+class _GameScreenState extends ConsumerState<GameScreen> {
+  late WebsocketNotifier webSocketNotifier;
   final _questionController = TextEditingController();
   final questions = dummy_questions;
   GameQuestion? currentQuestion;
@@ -25,7 +29,43 @@ class _GameScreenState extends State<GameScreen> {
   bool answerSubmitted = false;
   bool showingAnswers = false;
 
+  void submitAnswer() async {
+    final isNumeric =
+        RegExp(r'^-?\d+(\.\d+)?$').hasMatch(_questionController.text);
+
+    if (!isNumeric || _questionController.text.isEmpty) {
+      showAlertDialog(
+        "Σφάλμα απάντησης",
+        "Η απάντηση πρέπει να είναι μόνο αριθμοί",
+        context,
+      );
+      return;
+    }
+
+    final answer = double.parse(_questionController.text);
+    // todo: send websocket request to submit answer
+
+    // 1. send request
+    await webSocketNotifier.submitAnswer(answer, widget.gameId);
+    // 2. move to next state (answerSubmitted)
+    // todo: remove the following lines after making theresponse from the server
+    print(showingScores);
+    print(showingQuestion);
+    print(answerSubmitted);
+    print(showingAnswers);
+    setState(() {
+      showingScores = false;
+      showingQuestion = false;
+      answerSubmitted = true;
+      showingAnswers = false;
+    });
+  }
+
   void handleQuestion() {
+    print(showingScores);
+    print(showingQuestion);
+    print(answerSubmitted);
+    print(showingAnswers);
     // todo: get a random question from the db
     print(_questionController.text);
 
@@ -36,6 +76,8 @@ class _GameScreenState extends State<GameScreen> {
       answerSubmitted = false;
       showingAnswers = false;
     } else if (showingQuestion) {
+      // todo: remove this case after everything is set up correctly
+
       // todo: also check if given answer is numbers only
       if (_questionController.text.isEmpty) {
         showAlertDialog(
@@ -51,6 +93,7 @@ class _GameScreenState extends State<GameScreen> {
       answerSubmitted = true;
       showingAnswers = false;
     } else if (answerSubmitted) {
+      // todo: move this in build after checking the response to see if all players have answered
       // todo: get all answers from the question
       // todo: probably from redis?
       // todo: wait for all players to answer and then continue
@@ -77,6 +120,12 @@ class _GameScreenState extends State<GameScreen> {
   }
 
   @override
+  void initState() {
+    webSocketNotifier = ref.read(websocketProvider.notifier);
+    super.initState();
+  }
+
+  @override
   void dispose() {
     _questionController.dispose();
     super.dispose();
@@ -84,7 +133,27 @@ class _GameScreenState extends State<GameScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final response = ref.watch(websocketProvider);
     Widget content;
+    Widget? bottomButton;
+
+    if (response is AnswerSubmittedResponse) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        // move to answerSubmitted after getting the response
+        showingScores = false;
+        showingQuestion = false;
+        answerSubmitted = true;
+        showingAnswers = false;
+        ref.read(websocketProvider.notifier).reset();
+      });
+    }
+
+    if (response is PlayerAnsweredResponse) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        // todo: udpate submitted answers by adding checkmark icon next to player
+        ref.read(websocketProvider.notifier).reset();
+      });
+    }
 
     if (showingScores) {
       content = Column(
@@ -101,19 +170,29 @@ class _GameScreenState extends State<GameScreen> {
           ),
         ],
       );
+
+      bottomButton = ElevatedButton(
+        onPressed: handleQuestion,
+        child: const Text('Next Question'),
+      );
     } else if (showingQuestion) {
       content = Center(
         child: QuestionCard(
           gameQuestion: currentQuestion!,
           questionController: _questionController,
+          onSubmitAnswer: submitAnswer,
         ),
       );
+
+      bottomButton = null;
     } else if (answerSubmitted) {
       //todo: wait for all players to answer and then continue
       //todo: show check mark for players that have answered
       content = Center(
         child: Text('Answer submitted'),
       );
+
+      bottomButton = null;
     } else if (showingAnswers) {
       content = Center(
         // child: Text('Showing Answers'),
@@ -131,8 +210,14 @@ class _GameScreenState extends State<GameScreen> {
           ),
         ),
       );
+
+      bottomButton = ElevatedButton(
+        onPressed: handleQuestion,
+        child: const Text('Show Scores'),
+      );
     } else {
       content = Text('No content yet');
+      bottomButton = null;
     }
 
     return Scaffold(
@@ -155,6 +240,8 @@ class _GameScreenState extends State<GameScreen> {
                 ? const Text('')
                 : const Text('Show Scores'),
       ),
+      // todo: uncomment this after everythingis done
+      // bottomButton,
     );
   }
 }
