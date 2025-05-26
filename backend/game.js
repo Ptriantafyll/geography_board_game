814ecb1444da42f53d71a702e5390b6427f5ca39
ptriantafyll <ptriantafyll@gmail.com>
2025-05-26 20:12:49 +0300

    Removed all functions from base js file and made separate files

---
+++ backend/game.js
@@ -0,0 +1,192 @@
+// Start game
+async function startGame(websocket, data, pool, redis) {
+  try {
+    let gameId = uuidv4();
+    await pool.query("INSERT INTO Game (id) VALUES (?)", gameId);
+
+    let playersInLobbyResult = await pool.query(
+      "SELECT Player.name, Player.color, Player.id FROM Player " +
+        "JOIN Lobby_Player ON Player.id = Lobby_Player.player_id " +
+        "JOIN Lobby ON Lobby.id = Lobby_Player.lobby_id WHERE Lobby.id =?",
+      data.lobbyId
+    );
+
+    let playersInLobby = playersInLobbyResult[0];
+    console.log("players in lobby: ", playersInLobby);
+
+    for (player of playersInLobby) {
+      console.log("player: ", player);
+      await pool.query(
+        "INSERT INTO Game_Player (game_id, player_id) VALUES (?,?)",
+        [gameId, player.id]
+      );
+      redis.hset(`game:${gameId}:scores`, player.id, 0);
+      // creates scores in redis
+      console.log("set score 0 for plaer: ", player.id);
+    }
+
+    let gameStartedMessage = JSON.stringify({
+      type: "GAME_STARTED",
+      gameId: gameId,
+      requestId: data.id,
+    });
+
+    let targetIds = new Set(playersInLobby.map((player) => player.id));
+    console.log("targetIds: ", targetIds);
+
+    // todo: make this a function (broadcast?) that takes a lobby/game id
+    // todo: and sends a message to all players
+    for (const [playerId, websocket] of clients.entries()) {
+      console.log("client id: ", playerId);
+      if (targetIds.has(playerId) && websocket.readyState === websocket.OPEN) {
+        await websocket.send(gameStartedMessage);
+      }
+    }
+
+    console.log("sent: ", gameStartedMessage);
+  } catch (error) {
+    console.log("Error starting game: ", error);
+  }
+}
+
+// submit answer
+async function submitAnswer(websocket, playerId, data, pool, redis) {
+  // 1. get answer
+  let answer = data.answer;
+  let gameId = data.gameId;
+  // store answer into redis
+  await redis.hset(`game:${gameId}:answers`, playerId, answer);
+
+  // 2. get players' answers from redis using gameId
+  let answers = await redis.hgetall(`game:${gameId}:answers`);
+  console.log("answers before delete: ", answers);
+
+  //  i. get players from sql db (Game_Player will need data.gameId)
+  let playersInGameResult = await pool.query(
+    "SELECT Player.name, Player.color, Player.id FROM Player " +
+      "JOIN Game_Player ON Player.id = Game_Player.player_id " +
+      "JOIN Game ON Game.id = Game_Player.game_id WHERE Game.id =?",
+    gameId
+  );
+  let playersInGame = playersInGameResult[0];
+
+  //  ii. create object {} with players
+  let playersAnswered = {};
+  let playersWithAnswers = {};
+  for (let playerInGame of playersInGame) {
+    //  iii. "playerID": true/false if answered/not anwered yet (compare with answers from redis)
+    let playerHasAnswered = playerInGame.id in answers;
+    playersAnswered[playerInGame.id] = playerHasAnswered;
+
+    if (playerInGame.id in answers) {
+      // iv. "playerID": answer
+      playersWithAnswers[playerInGame.id] = answers[playerInGame.id];
+    }
+  }
+
+  // 3. respond to player that answered with type "ANSWER_SUBMITTED" and with an array with the players that have answered
+  let playerSubmitAnswerMessage = JSON.stringify({
+    type: "ANSWER_SUBMITTED",
+    playersAnswered: playersAnswered,
+    playersWithAnswers: playersWithAnswers,
+    requestId: data.id,
+  });
+
+  await websocket.send(playerSubmitAnswerMessage);
+  console.log("sent: ", playerSubmitAnswerMessage);
+
+  // 4. notify all other players that this player has answered
+  let playerAnsweredMessage = JSON.stringify({
+    type: "PLAYER_ANSWERED",
+    playerAnswered: playerId,
+    answer: answer,
+    requestId: data.id,
+  });
+
+  let targetIds = new Set(playersInGame.map((player) => player.id));
+  console.log("targetIds: ", targetIds);
+
+  for (const [player_Id, websocket] of clients.entries()) {
+    if (player_Id === playerId) continue;
+
+    if (targetIds.has(player_Id) && websocket.readyState === websocket.OPEN) {
+      await websocket.send(playerAnsweredMessage);
+    }
+  }
+
+  // clear all answers from redis after all players have submitted
+  console.log("players with answers: ", playersWithAnswers);
+  console.log(
+    "players with answers length: ",
+    Object.keys(playersWithAnswers).length
+  );
+  console.log("players in game: ", playersInGame);
+  console.log("players in game length: ", playersInGame.length);
+
+  if (Object.keys(playersWithAnswers).length === playersInGame.length) {
+    await redis.del(`game:${gameId}:answers`);
+    console.log(
+      "All players have answered this round. Deleting answers from redis"
+    );
+  }
+}
+
+async function showQuestion(websocket, data, pool) {
+  let gameId = data.gameId;
+
+  let numOfQuestionsResult = await pool.query("SELECT COUNT(id) FROM Question");
+  let numOfQuestions = numOfQuestionsResult[0][0]["COUNT(id)"];
+  let questionId = getRandomInt(numOfQuestions);
+
+  let questionResult = await pool.query(
+    "SELECT text, answer FROM Question WHERE id=?",
+    questionId
+  );
+
+  let questionToSend = {
+    text: questionResult[0][0]["text"],
+    answer: questionResult[0][0]["answer"].toString(),
+  };
+  // {
+  //   text: "How tall is mount everest? (in meters)",
+  //   answer: "8849.0",
+  // };
+
+  // 2. get all players in the game
+  let playersInGameResult = await pool.query(
+    "SELECT Player.name, Player.color, Player.id FROM Player " +
+      "JOIN Game_Player ON Player.id = Game_Player.player_id " +
+      "JOIN Game ON Game.id = Game_Player.game_id WHERE Game.id =?",
+    gameId
+  );
+  let playersInGame = playersInGameResult[0];
+
+  // 3. send message to all players with the question
+  let showQuestionMessage = JSON.stringify({
+    type: "QUESTION_SHOWN",
+    question: questionToSend,
+    requestId: data.id,
+  });
+  console.log("sending: ", showQuestionMessage);
+
+  let targetIds = new Set(playersInGame.map((player) => player.id));
+  console.log("targetIds: ", targetIds);
+
+  for (const [playerId, websocket] of clients.entries()) {
+    console.log("client id: ", playerId);
+    if (targetIds.has(playerId) && websocket.readyState === websocket.OPEN) {
+      await websocket.send(showQuestionMessage);
+    }
+  }
+}
+
+async function updateScores(websocket, data) {
+  // 1. get scores from request
+  // 2. update scores in redis to add 1 to the round winner
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
+
+module.exports = { startGame, submitAnswer, showQuestion, updateScores };
--- backend/player.js
+++ backend/player.js
@@ -1,3 +1,26 @@
+// Creates a new player
+async function createPlayer(websocket, playerId, data, pool) {
+  try {
+    playerName = data.name;
+    playerColor = data.color;
+    playerCreatedMessage = JSON.stringify({
+      type: "PLAYER_CREATED",
+      playerId: playerId,
+      requestId: data.id,
+    });
+
+    await pool.query("INSERT INTO Player (id, name, color) VALUES (?, ?, ?)", [
+      playerId,
+      playerName,
+      playerColor,
+    ]);
+    await websocket.send(playerCreatedMessage);
+    console.log("sent ", playerCreatedMessage);
+  } catch (error) {
+    console.error("Error creating Player", error);
+  }
+}
+
 // Deletes a player
 async function deletePlayer(websocket, playerId, data, pool) {
   try {
@@ -41,4 +64,4 @@ async function deletePlayer(websocket, playerId, data, pool) {
   }
 }
 
-module.exports = deletePlayer;
+module.exports = { createPlayer, deletePlayer };
